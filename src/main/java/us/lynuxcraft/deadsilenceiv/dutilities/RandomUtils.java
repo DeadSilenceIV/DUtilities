2b73b59f36ba46dd728a660768225ff18ad09197
DeadSilenceIV <barreto-h2@hotmail.com>
2023-01-01 08:09:53 -0500

    [NEW] Changes were done to the RandomUtils class.

--- src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/RandomUtils.java
+++ src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/RandomUtils.java
@@ -4,14 +4,21 @@ import java.util.Random;
 
 public class RandomUtils {
 
-    private static Random random = new Random();
+    private static Random random;
 
     public static int getRandomIntBetween(int minimum, int maximum) {
-        return random.nextInt(maximum - minimum) + minimum;
+        return getRandom().nextInt(maximum - minimum) + minimum;
     }
 
     public static double getRandomDoubleBetween(double minimum, double maximum) {
-        return random.nextDouble() * (maximum - minimum) + minimum;
+        return getRandom().nextDouble() * (maximum - minimum) + minimum;
+    }
+
+    public static Random getRandom(){
+        if(random == null){
+            random = new Random();
+        }
+        return random;
     }
 
 }
