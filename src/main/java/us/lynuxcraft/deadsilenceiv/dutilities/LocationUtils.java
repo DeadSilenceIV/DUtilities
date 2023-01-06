4c45fbd44303d4af06fa5968abc4b682acc36f85
DeadSilenceIV <barreto-h2@hotmail.com>
2023-01-05 20:32:06 -0500

    [NEW] A new class was added to the library.

---
+++ src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/LocationUtils.java
@@ -0,0 +1,45 @@
+package us.lynuxcraft.deadsilenceiv.dutilities;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class LocationUtils {
+
+    public static String serializeLoc(Location l) {
+        StringBuilder s = new StringBuilder();
+        s.append("@w;").append(l.getWorld().getName());
+        s.append(":@x;").append(l.getX());
+        s.append(":@y;").append(l.getY());
+        s.append(":@z;").append(l.getZ());
+        s.append(":@pi;").append(l.getPitch());
+        s.append(":@ya;").append(l.getYaw());
+        return s.toString();
+    }
+
+    public static Location deserializeLoc(String s) {
+        Location l = new Location(Bukkit.getWorlds().get(0), 0, 0, 0,0,0);
+        String[] att = s.split(":");
+        for(String attribute : att) {
+            String[] split = attribute.split(";");
+            if(split[0].equalsIgnoreCase("@w"))
+                l.setWorld(Bukkit.getWorld(split[1]));
+            if(split[0].equalsIgnoreCase("@x"))
+                l.setX(Double.parseDouble(split[1]));
+            if(split[0].equalsIgnoreCase("@y"))
+                l.setY(Double.parseDouble(split[1]));
+            if(split[0].equalsIgnoreCase("@z"))
+                l.setZ(Double.parseDouble(split[1]));
+            if(split[0].equalsIgnoreCase("@pi"))
+                l.setPitch((float) Double.parseDouble(split[1]));
+            if(split[0].equalsIgnoreCase("@ya"))
+                l.setYaw((float) Double.parseDouble(split[1]));
+
+        }
+        return l;
+    }
+
+    public static boolean verify(Location location){
+        return (location != null && location.getWorld() != null);
+    }
+
+}
