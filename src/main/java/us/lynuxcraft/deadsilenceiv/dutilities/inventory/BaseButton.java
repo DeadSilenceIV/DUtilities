13bdc7fd980f0cadefb78d6e40c3210c37606c30
DeadSilenceIV <barreto-h2@hotmail.com>
2023-01-22 14:56:36 -0500

    [NEW] A new constructor was added to the BaseButton class.

--- src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/inventory/BaseButton.java
+++ src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/inventory/BaseButton.java
@@ -13,8 +13,13 @@ public class BaseButton<T extends InteractiveItem> implements Button<T> {
     @Getter @Setter protected int slot;
     @Getter @Setter protected T currentItem;
     public BaseButton(String name,int slot) {
-        this.name = name;
+        this(name);
         this.slot = slot;
+    }
+
+    public BaseButton(String name){
+        this.name = name;
         this.items = new HashSet<>();
     }
+
 }
