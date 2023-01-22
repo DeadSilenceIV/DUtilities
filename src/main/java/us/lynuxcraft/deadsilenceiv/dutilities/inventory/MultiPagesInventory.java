c1f6792af3527aeaf49a20058495aa0ea8870535
DeadSilenceIV <barreto-h2@hotmail.com>
2023-01-22 07:37:26 -0500

    [NEW] The method resize of the class MultiPagesInventory was modified.

--- src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/inventory/MultiPagesInventory.java
+++ src/main/java/us/lynuxcraft/deadsilenceiv/dutilities/inventory/MultiPagesInventory.java
@@ -2,11 +2,9 @@ package us.lynuxcraft.deadsilenceiv.dutilities.inventory;
 
 import lombok.Getter;
 import org.bukkit.entity.HumanEntity;
+import us.lynuxcraft.deadsilenceiv.dutilities.Pair;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 public abstract class MultiPagesInventory<T extends InventoryPage>{
     protected UUID uuid;
@@ -37,24 +35,32 @@ public abstract class MultiPagesInventory<T extends InventoryPage>{
         this.size = size;
     }
 
-    protected void resize(int newSize){
+    protected Pair<ResizeResult, Set<T>> resize(int newSize){
         int numberOfPages = (int) Math.floor((double) newSize/ (double) 45);
         int totalRows = (newSize/9);
         int pagesToCreate = (newSize % 45 == 0) ? (numberOfPages-1) : numberOfPages;
         int lastPageSlots = (newSize % 45 != 0) ? (totalRows-(numberOfPages*5))*9 : 45;
+        ResizeResult result;
+        Set<T> modified = new HashSet<>();
         if(numberOfPages > pages.size()){
+            result = ResizeResult.ADDED_PAGES;
             for (int i = pages.size()-1; i < numberOfPages; i++) {
-                pages.put(i,i < pagesToCreate ? newPage(i, 45) : newPage(i, lastPageSlots));
+                T added = pages.put(i,i < pagesToCreate ? newPage(i, 45) : newPage(i, lastPageSlots));
+                modified.add(added);
             }
         }else if(numberOfPages < pages.size()){
+            result = ResizeResult.REMOVED_PAGES;
             for (int i = numberOfPages-1; i < pages.size(); i++) {
-                pages.remove(i);
+                T removed = pages.remove(i);
+                modified.add(removed);
             }
             changeLastPageSlots(lastPageSlots);
         }else{
+            result = ResizeResult.NOTHING;
             changeLastPageSlots(lastPageSlots);
         }
         this.size = newSize;
+        return new Pair<>(result,modified);
     }
 
     protected void changeLastPageSlots(int newAmount){
@@ -92,5 +98,11 @@ public abstract class MultiPagesInventory<T extends InventoryPage>{
             cachedHashCode =  Objects.hash(uuid);
         }
         return cachedHashCode;
+    }
+
+    protected enum ResizeResult{
+        ADDED_PAGES,
+        REMOVED_PAGES,
+        NOTHING
     }
 }
